cb8cfaf000c62fdbd8807e19aca9a80a3a5a8df1
anikrauth <anikrouthwebdev@gmail.com>
2023-12-06 15:55:20 +0600

    fixed

--- src/lib/ApolloClient.tsx
+++ src/lib/ApolloClient.tsx
@@ -1,5 +1,5 @@
 import { GraphQLClient } from 'graphql-request'
 
-const client = new GraphQLClient(`${process.env.NEXT_PUBLIC_BACKEND_URI}/graphql`)
+const client = new GraphQLClient(`${process.env.NEXT_PUBLIC_BACKEND_URI || 'https://backend.presenttill.nu' }/graphql`)
 
 export default client;
